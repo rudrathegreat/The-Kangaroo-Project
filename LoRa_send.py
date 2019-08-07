1adcedb95ca6cb17e55c8b5cfafb2785a7398c20
Rudra <37140269+rudrathegreat@users.noreply.github.com>
2019-08-07 20:50:05 +1000

    Added Code

---
+++ LoRa_send.py
@@ -0,0 +1,20 @@
+from network import LoRa
+import socket
+import time
+import pycom
+
+lora = LoRa(mode=LoRa.LORA, region=LoRa.AU195)
+
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setblocking(False)
+pycom.heartbeat(False)
+
+while True:
+	s.send('Ping')
+	print('Ping')
+	pycom.rgbled(0xFF0000)
+	time.sleep(1)
+
+	pycom.rgbled(0x0000FF)
+	print('Pause')
+	time.sleep(4)
